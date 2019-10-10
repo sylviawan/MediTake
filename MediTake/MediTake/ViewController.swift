369fea6793223eaa08df2c02dc4627b5d990d090
Sylvia Wan <sylvia.wan97@gmail.com>
2019-10-11 00:00:26 +0100

    First commit
    Deciding to develop the app with Swift

---
+++ MediTake/MediTake/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MediTake
+//
+//  Created by Sylvia Wan on 10/10/2019.
+//  Copyright © 2019 Sylvia Wan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
